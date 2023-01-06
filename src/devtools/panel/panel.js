1b985c5e682faa5445643ee28775085931f8122c
Jeff Griffiths <jeff@eko-recordings.ca>
2023-01-06 14:36:57 -0800

    inspectedWindow actually works yaya

--- src/devtools/panel/panel.js
+++ src/devtools/panel/panel.js
@@ -38,6 +38,13 @@ document.getElementById('ping').addEventListener('click', () => {
   })
 });
 
+document.getElementById('page-info').addEventListener('click', () => {
+  getPageInfo((msg) => {
+    // console.log(`getPageInfo::${msg}`);
+    logInPage(`getPageInfo::${msg}`, 'log', true) // should be logged on both sides.
+  })
+});
+
 function handleMessage(request, sender, sendResponse) {
   console.log('from background page>', request, sender, sendResponse)
 }
@@ -51,4 +58,38 @@ setTimeout(() => {
   injectScript('/devtools/panel/content_script.js')
 }, 0);
 
+function getPageInfo(callback) {
+  chrome.devtools.inspectedWindow.eval(
+    "navigator.webnative.version",
+    function(result, isException) {
+      if (isException) {
+        callback("Webnative is not loaded.");
+      } else {
+        callback(`Webnative version ${result} is loaded.`);
+      }
+    }
+  );
+}
 
+function logInPage(message, level, debug) {
+  let levels = [ 'log', 'warn', 'error', 'info' ];
+
+  if (!levels.includes(level)) {
+    console.warn(`Invalid level supplied: ${level}, needs to be one of ${levels.join(',')}`);
+    level = 'log'; // default log level
+  }
+
+  chrome.devtools.inspectedWindow.eval(
+    `console.${level}('From devtools: ${message}')`,
+    function (result, isException) {
+      if (isException) {
+        console.error(`Exception thrown trying to log to the page: ${result}`)
+      }
+      else {
+        if (debug === true) {
+          console.log(`DEBUG: Logged ${message} to the page console.`)
+        }
+      }
+    }
+  );  
+}
--- tmp/readme.md
+++
@@ -1,5 +0,0 @@
-# Webnative Developer Tools
-
-You're here for a browser developer tools extension that helps you understand wtf is going on when developing a webnative-based app.
-
-
