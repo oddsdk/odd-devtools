aeb7069c1224f72a07314a36fabced9ccab4530d
Brian Ginsburg <gins@brianginsburg.com>
2023-01-14 10:29:20 -0800

    Add chrome build

---
+++ vite.chrome.config.ts
@@ -0,0 +1,21 @@
+import { crx } from "@crxjs/vite-plugin";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import manifest from "./manifest-chrome.json";
+
+const srcDir = resolve(__dirname, "src");
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    build: {
+        emptyOutDir: true,
+        outDir: resolve(__dirname, 'dist/chrome')
+    },
+    plugins: [ svelte(), crx({ manifest }) ],
+    resolve: {
+        alias: {
+            src: srcDir,
+        },
+    },
+})
