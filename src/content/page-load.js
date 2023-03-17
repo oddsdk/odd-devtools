98a44468d20318f104e7f13dca00d84705906112
Brian Ginsburg <gins@brianginsburg.com>
2023-03-17 08:46:35 -0700

    Clean up console logs

--- src/content/page-load.js
+++ src/content/page-load.js
@@ -3,8 +3,6 @@ window.addEventListener('message', (event) => {
   if (event.source !== window) return
 
   if (event.data.id === 'webnative-devtools-ready-message') {
-    // console.log('in page load content script, got message', event.data)
-
     chrome.runtime.sendMessage({ id: `${chrome.runtime.id}`, type: 'pageload' })
   }
 })
