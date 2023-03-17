90c646dc7617fb85a1836db7d3235ea05a4f1942
Brian Ginsburg <gins@brianginsburg.com>
2023-03-17 10:51:53 -0700

    Add icons

---
+++ src/devtools/panel/icons/EllipseSolid.svelte
@@ -0,0 +1,9 @@
+<svg
+  width="13"
+  height="14"
+  viewBox="0 0 13 14"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <circle cx="6.12122" cy="7.00696" r="6.12122" fill="black" />
+</svg>
---
+++ src/devtools/panel/icons/Search.svelte
@@ -0,0 +1,22 @@
+<svg
+  width="12"
+  height="13"
+  viewBox="0 0 12 13"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    d="M5.5 10.0068C7.70914 10.0068 9.5 8.21597 9.5 6.00684C9.5 3.7977 7.70914 2.00684 5.5 2.00684C3.29086 2.00684 1.5 3.7977 1.5 6.00684C1.5 8.21597 3.29086 10.0068 5.5 10.0068Z"
+    stroke="white"
+    stroke-width="0.75"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M10.5 11.0068L8.32495 8.83179"
+    stroke="white"
+    stroke-width="0.75"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+</svg>
