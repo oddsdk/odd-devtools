bbeda715db753fbd7fe0f086913c87261bf7d47b
Brian Ginsburg <gins@brianginsburg.com>
2023-03-22 08:46:34 -0700

    Add Webnative connected nav title

--- src/devtools/Panel.svelte
+++ src/devtools/Panel.svelte
@@ -2,7 +2,7 @@
   import { onDestroy } from 'svelte'
 
   import './panel/panel.css'
-  import { namespaceStore } from './panel'
+  import { connectionStore, namespaceStore } from './panel'
   import Events from './panel/Events.svelte'
   import Namespaces from './panel/Namespaces.svelte'
   import Nav from './panel/Nav.svelte'
@@ -28,7 +28,7 @@
 <div
   class="min-h-screen min-w-screen grid grid-rows-[32px_auto] divide-y divide-[#4A4C50] text-white bg-black"
 >
-  <Nav />
+  <Nav connection={$connectionStore} />
   <div class="grid grid-cols-[1fr_4fr] divide-x divide-[#4A4C50]">
     <Namespaces {namespaces} on:change={handleNamespaceChange} />
     <Events />
--- src/devtools/panel/Nav.svelte
+++ src/devtools/panel/Nav.svelte
@@ -2,12 +2,21 @@
   import TrashIcon from './icons/Trash.svelte'
   import MoreVerticalIcon from './icons/MoreVertical.svelte'
   import WebnativeIcon from './icons/Webnative.svelte'
+
+  export let connection
 </script>
 
 <div class="grid grid-cols-[4fr_1fr] justify-center">
   <div class="flex flex-row gap-2 px-4 py-2 justify-start">
     <WebnativeIcon />
-    <span class="text-xs font-medium">Webnative</span>
+    <span class="text-xs font-medium">
+      Webnative
+      {#if connection.connected}
+        Connected
+      {:else}
+        Not Connected
+      {/if}
+    </span>
   </div>
   <div class="flex flex-row gap-2 py-2 justify-end">
     <div class="flex flex-row gap-2">
