d13c38849af980dfab5fe23e8f23b29c653dac62
Brian Ginsburg <gins@brianginsburg.com>
2023-03-27 09:31:04 -0700

    Set not connected status at page load
    Connect on ready message sent when Webnative is available and in debug
    mode.

--- src/content/page-load.js
+++ src/content/page-load.js
@@ -1,8 +1,10 @@
+chrome.runtime.sendMessage({ id: `${chrome.runtime.id}`, type: 'pageload' })
+
 window.addEventListener('message', (event) => {
   // Reject messages not from ourselves
   if (event.source !== window) return
 
   if (event.data.id === 'webnative-devtools-ready-message') {
-    chrome.runtime.sendMessage({ id: `${chrome.runtime.id}`, type: 'pageload' })
+    chrome.runtime.sendMessage({ id: `${chrome.runtime.id}`, type: 'ready' })
   }
 })
