0a4534622c0f04336e32298115ebde364ceab08d
Brian Ginsburg <gins@brianginsburg.com>
2023-04-25 14:03:01 -0700

    Add debug off and connection failed views

---
+++ src/devtools/panel/errors/ConnectionFailed.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { fly } from 'svelte/transition'
+  import { quintIn } from 'svelte/easing'
+
+  export let error: string
+</script>
+
+<div class="grid justify-center items-center">
+  <div
+    class="grid grid-align-row grid-rows-2 max-w-[400px] justify-center"
+    in:fly={{ duration: 275, easing: quintIn }}
+  >
+    <div
+      class="h-[39px] text-[26px] tracking-[-0.011em] text-center font-bold text-red-500"
+    >
+      Unable to connect to the ODD SDK
+    </div>
+    <div class="text-center text-xs text-medium font-mono">
+      Please open an issue in the
+      <a
+        class="text-blue-500"
+        href="https://github.com/oddsdk/odd-devtools/issues"
+        target="_blank"
+        rel="noreferrer"
+      >
+        ODD Devtools issue tracker
+      </a>
+      and include the following error message:
+    </div>
+    <div class="text-center text-xs text-medium font-mono text-red-500">
+      {error}
+    </div>
+  </div>
+</div>
