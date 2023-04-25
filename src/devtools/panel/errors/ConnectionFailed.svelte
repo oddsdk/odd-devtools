414b4507135da6288b771c9c47ef50081061d6f4
Brian Ginsburg <gins@brianginsburg.com>
2023-04-25 14:37:00 -0700

    Adjust dark theme colorings

--- src/devtools/panel/errors/ConnectionFailed.svelte
+++ src/devtools/panel/errors/ConnectionFailed.svelte
@@ -11,14 +11,14 @@
     in:fly={{ duration: 275, easing: quintIn }}
   >
     <div
-      class="h-[39px] text-[26px] tracking-[-0.011em] text-center font-bold text-red-500"
+      class="h-[39px] text-[26px] tracking-[-0.011em] text-center font-bold text-red-500 dark:text-red-400"
     >
       Unable to connect to the ODD SDK
     </div>
     <div class="text-center text-xs text-medium font-mono">
       Please open an issue in the
       <a
-        class="text-blue-500"
+        class="text-blue-500 dark:text-blue-400"
         href="https://github.com/oddsdk/odd-devtools/issues"
         target="_blank"
         rel="noreferrer"
@@ -27,7 +27,9 @@
       </a>
       and include the following error message:
     </div>
-    <div class="text-center text-xs text-medium font-mono text-red-500">
+    <div
+      class="text-center text-xs text-medium font-mono text-red-500 dark:text-red-400"
+    >
       {error}
     </div>
   </div>
--- src/devtools/panel/errors/DebugOff.svelte
+++ src/devtools/panel/errors/DebugOff.svelte
@@ -9,14 +9,14 @@
     in:fly={{ duration: 275, easing: quintIn }}
   >
     <div
-      class="h-[39px] text-[26px] tracking-[-0.011em] text-center font-bold text-red-500"
+      class="h-[39px] text-[26px] tracking-[-0.011em] text-center font-bold text-red-500 dark:text-red-400"
     >
       Unable to connect to the ODD SDK
     </div>
     <div class="text-center text-xs text-medium font-mono">
       Please make sure the ODD SDK is present on this page and
       <a
-        class="text-blue-500"
+        class="text-blue-500 dark:text-blue-400"
         href="https://docs.odd.dev/additional-info#program"
         target="_blank"
         rel="noreferrer"
