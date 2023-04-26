8d07a11433a1949b0dbe3207d1ecf048a38a01d8
Brian Ginsburg <gins@brianginsburg.com>
2023-04-26 11:42:51 -0700

    Remove console logs

--- src/devtools/devtools.js
+++ src/devtools/devtools.js
@@ -3,8 +3,6 @@ import browser from 'webextension-polyfill'
 
 import { allNamespace, namespaceToString } from '../namespace'
 
-console.log(`In devtools.js - tabId: ${browser.devtools.inspectedWindow.tabId}`)
-
 const tabId = browser.devtools.inspectedWindow.tabId
 
 // INIT 
@@ -84,8 +82,6 @@ connect()
  * Rewire connection with the background script on message
  */
 function handleBackgroundConnection(port) {
-  // console.log('connection in devtools page from ', port.name)
-
   if (port.name === `odd_background-${tabId}`) {
     backgroundPort = port
     backgroundPort.onMessage.addListener(handleBackgroundMessage)
@@ -100,8 +96,6 @@ chrome.runtime.onConnect.addListener(handleBackgroundConnection)
 export const connectionStore = writable({ tabId, connected: false, error: null })
 
 export async function connect() {
-  console.log('Connecting to the ODD SDK')
-
   const [connecting, err] = await browser.devtools.inspectedWindow.eval(`
     if (window.__odd?.extension) {
       window.__odd.extension.connect('${chrome.runtime.id}')
@@ -122,8 +116,6 @@ export async function connect() {
 }
 
 export async function disconnect() {
-  console.log('Disconnecting from the ODD SDK')
-
   const [disconnecting, err] = await browser.devtools.inspectedWindow.eval(`
     if (window.__odd?.extension) {
       window.__odd.extension.disconnect('${chrome.runtime.id}')
@@ -150,11 +142,7 @@ export const messageStore = writable([])
 export const namespaceStore = writable([])
 
 function handleBackgroundMessage(message) {
-  // console.log('message received from tab', tabId,'in devtools panel', message)
-
   if (message.type === 'connect') {
-    console.log('Received connect message from the ODD SDK', message)
-
     const namespace = {
       namespace: namespaceToString(message.state.app.namespace),
       version: message.state.odd.version
@@ -165,20 +153,12 @@ function handleBackgroundMessage(message) {
 
     connectionStore.update(store => ({ ...store, connected: true }))
   } else if (message.type === 'disconnect') {
-    console.log('Received disconnect message from the ODD SDK', message)
-
     connectionStore.update(store => ({ ...store, connected: false }))
   } else if (message.type === 'session') {
-    console.log('Received session message', message)
-
     messageStore.update(history => [...history, message])
   } else if (message.type === 'fileSystem') {
-    console.log('Received file system message', message)
-
     messageStore.update(history => [...history, message])
   } else if (message.type === 'pageload') {
-    console.log('Received page load message', message)
-
     connectionStore.update(store => ({ ...store, connected: false }))
 
     // Assume debug mode off if no connect message received
@@ -190,8 +170,6 @@ function handleBackgroundMessage(message) {
       }
     }, 1000)
   } else if (message.type === 'ready') {
-    console.log('Received ready message', message)
-
     // Inject content script if missing
     backgroundPort.postMessage({
       type: 'inject',
